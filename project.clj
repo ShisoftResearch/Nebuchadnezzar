f98fae510dd95c5e402690c50cc3c606ea9204ee
Jack Shi <shisoftgenius@gmail.com>
2016-01-18 01:09:39 +0800

    rearrange code root, gitignore idea folder

--- project.clj
+++ project.clj
@@ -3,4 +3,6 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :source-paths ["src/clj"]
+  :java-source-paths ["src/java"]
   :dependencies [[org.clojure/clojure "1.7.0"]])
