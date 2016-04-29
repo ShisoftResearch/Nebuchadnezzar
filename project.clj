0ade841fb2188ce75e61748be6bec5933fd19e51
John Shi <shisoftgenius@gmail.com>
2016-04-29 17:19:54 +0800

    bump clojure version

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :source-paths ["src/clj"]
   :java-source-paths ["src/java"]
-  :dependencies [[org.clojure/clojure "1.7.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [aleph "0.4.1-beta2"]
                  [cluster-connector "0.1.0-SNAPSHOT"]
                  [net.openhft/koloboke-api-jdk8 "0.6.8"]
