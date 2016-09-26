256ceba5a26696a0800c2ef8eb40c74bed2b38c9
Jack Shi <shisoftgenius@gmail.com>
2016-09-27 06:30:56 +0800

    type alias

--- src/ram/types.rs
+++ src/ram/types.rs
@@ -45,6 +45,15 @@ macro_rules! define_types {
                 _ => "N/A",
            }
         }
+        fn get_size (id: i32) -> usize {
+           match id {
+                $(
+                    $id => $size,
+                )*
+                _ => 0,
+           }
+        }
+
     );
 }
 
@@ -52,7 +61,7 @@ macro_rules! rc {
     (
         $read:ident
     ) => (
-        {|pos| -> $read(pos)}
+        $read
     );
 }
 
@@ -60,7 +69,7 @@ macro_rules! wc {
     (
         $rwrite:ident
     ) => (
-        {|pos, val| -> $write(pos, val)}
+        $rwrite
     );
 }
 
@@ -91,5 +100,17 @@ gen_primitive_types_io!(
 
 define_types!(
     ["bool", "bit"], 0, rc!(read_bool), wc!(write_bool), size_of!(bool);
-    ["char"], 1, rc!(read_char), wc!(write_cahr), size_of!(char)
+    ["char"], 1, rc!(read_char), wc!(write_cahr), size_of!(char);
+    ["i8"], 2, rc!(read_i8), wc!(write_i8), size_of!(i8);
+    ["i16", "int"], 3, rc!(read_i16), wc!(write_i16), size_of!(i16);
+    ["i32", "long"], 4, rc!(read_i32), wc!(write_i32), size_of!(i32);
+    ["i64", "longlong"], 5, rc!(read_i16), wc!(write_i16), size_of!(i16);
+    ["u8", "byte"], 6, rc!(read_u8), wc!(write_u8), size_of!(u8);
+    ["u16"], 7, rc!(read_u16), wc!(write_u16), size_of!(u16);
+    ["u32"], 8, rc!(read_u32), wc!(write_u32), size_of!(u32);
+    ["u64"], 9, rc!(read_u64), wc!(write_u64), size_of!(u64);
+    ["isize"], 10, rc!(read_isize), wc!(write_isize), size_of!(isize);
+    ["usize"], 11, rc!(read_usize), wc!(write_usize), size_of!(usize);
+    ["f32", "float"], 12, rc!(read_f32), wc!(write_f32), size_of!(f32);
+    ["f64", "double"], 13, rc!(read_f64), wc!(write_f64), size_of!(f64)
 );
