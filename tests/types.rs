c1493335bd5a516f6dcdc37324c6f4127b452c35
Jack Shi <shisoftgenius@gmail.com>
2016-10-10 17:46:18 +0800

    first number type test

--- tests/types.rs
+++ tests/types.rs
@@ -1,4 +1,26 @@
+use neb::ram::types;
+use neb::ram::chunk;
+use std::*;
+
+macro_rules! test_nums {
+    (
+        $sym:ident, $io:ident, $name:ident
+    ) => (
+        #[test]
+        fn $name () {
+            let test_data = vec![$sym::MIN, $sym::MAX, 0, 1, 2, 255];
+            let chunk = &chunk::init(1, 2048).list[0];
+            for d in test_data {
+                types::$io::write(d, chunk.addr);
+                assert!(types::$io::read(chunk.addr) == d);
+            }
+        }
+    );
+}
+
 #[test]
 fn init () {
 
 }
+
+test_nums!(i8, i8_io, i8_test);
