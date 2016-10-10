8c1b8e21d1191eafe3e8502eac1a1562ffec7354
Jack Shi <shisoftgenius@gmail.com>
2016-10-11 00:00:17 +0800

    layout tests for compound types

--- tests/types.rs
+++ tests/types.rs
@@ -63,9 +63,17 @@ mod pos2d32 {
             types::pos2d32 {x: rand::random::<f32>(), y: rand::random::<f32>()}
         ];
         let chunk = &chunk::init(1, CHUNK_SIZE).list[0];
+        let counts = CHUNK_SIZE / types::pos2d32_io::size(0) as usize;
         for d in test_data {
-            types::pos2d32_io::write(&d, chunk.addr);
-            assert!(types::pos2d32_io::read(chunk.addr) == d);
+            for i in 0..counts {
+                let addr = chunk.addr + i * types::pos2d32_io::size(0);
+                types::pos2d32_io::write(&d, addr);
+                assert!(types::pos2d32_io::read(addr) == d);
+            }
+            for i in 0..counts {
+                let addr = chunk.addr + i * types::pos2d32_io::size(0);
+                assert!(types::pos2d32_io::read(addr) == d);
+            }
         }
     }
 }
@@ -75,16 +83,25 @@ mod pos2d64 {
     use neb::ram::chunk;
     use std;
     use rand;
+    use super::CHUNK_SIZE;
     #[test]
     fn test () {
         let test_data = vec![
             types::pos2d64 {x: std::f64::MIN, y: std::f64::MAX},
             types::pos2d64 {x: rand::random::<f64>(), y: rand::random::<f64>()}
         ];
-        let chunk = &chunk::init(1, 2048).list[0];
+        let chunk = &chunk::init(1, CHUNK_SIZE).list[0];
+        let counts = CHUNK_SIZE / types::pos2d64_io::size(0) as usize;
         for d in test_data {
-            types::pos2d64_io::write(&d, chunk.addr);
-            assert!(types::pos2d64_io::read(chunk.addr) == d);
+            for i in 0..counts {
+                let addr = chunk.addr + i * types::pos2d64_io::size(0);
+                types::pos2d64_io::write(&d, addr);
+                assert!(types::pos2d64_io::read(addr) == d);
+            }
+            for i in 0..counts {
+                let addr = chunk.addr + i * types::pos2d64_io::size(0);
+                assert!(types::pos2d64_io::read(addr) == d);
+            }
         }
     }
 }
@@ -94,16 +111,25 @@ mod pos3d32 {
     use neb::ram::chunk;
     use std;
     use rand;
+    use super::CHUNK_SIZE;
     #[test]
     fn test () {
         let test_data = vec![
             types::pos3d32 {x: std::f32::MIN, y: std::f32::MAX, z: rand::random::<f32>()},
             types::pos3d32 {x: rand::random::<f32>(), y: rand::random::<f32>(), z: rand::random::<f32>()}
         ];
-        let chunk = &chunk::init(1, 2048).list[0];
+        let chunk = &chunk::init(1, CHUNK_SIZE).list[0];
+        let counts = CHUNK_SIZE / types::pos3d32_io::size(0) as usize;
         for d in test_data {
-            types::pos3d32_io::write(&d, chunk.addr);
-            assert!(types::pos3d32_io::read(chunk.addr) == d);
+            for i in 0..counts {
+                let addr = chunk.addr + i * types::pos3d32_io::size(0);
+                types::pos3d32_io::write(&d, addr);
+                assert!(types::pos3d32_io::read(addr) == d);
+            }
+            for i in 0..counts {
+                let addr = chunk.addr + i * types::pos3d32_io::size(0);
+                assert!(types::pos3d32_io::read(addr) == d);
+            }
         }
     }
 }
@@ -113,16 +139,25 @@ mod pos3d64 {
     use neb::ram::chunk;
     use std;
     use rand;
+    use super::CHUNK_SIZE;
     #[test]
     fn test () {
         let test_data = vec![
             types::pos3d64 {x: std::f64::MIN, y: std::f64::MAX, z: rand::random::<f64>()},
             types::pos3d64 {x: rand::random::<f64>(), y: rand::random::<f64>(), z: rand::random::<f64>()}
         ];
-        let chunk = &chunk::init(1, 2048).list[0];
+        let chunk = &chunk::init(1, CHUNK_SIZE).list[0];
+        let counts = CHUNK_SIZE / types::pos3d64_io::size(0) as usize;
         for d in test_data {
-            types::pos3d64_io::write(&d, chunk.addr);
-            assert!(types::pos3d64_io::read(chunk.addr) == d);
+            for i in 0..counts {
+                let addr = chunk.addr + i * types::pos3d64_io::size(0);
+                types::pos3d64_io::write(&d, addr);
+                assert!(types::pos3d64_io::read(addr) == d);
+            }
+            for i in 0..counts {
+                let addr = chunk.addr + i * types::pos3d64_io::size(0);
+                assert!(types::pos3d64_io::read(addr) == d);
+            }
         }
     }
 }
@@ -132,6 +167,7 @@ mod uuid {
     use neb::ram::chunk;
     use uuid::Uuid;
     use uuid;
+    use super::CHUNK_SIZE;
     #[test]
     fn test () {
         let test_data = vec![
@@ -139,7 +175,7 @@ mod uuid {
             Uuid::new_v4(),
             Uuid::new_v5(&uuid::NAMESPACE_DNS, "foo")
         ];
-        let chunk = &chunk::init(1, 2048).list[0];
+        let chunk = &chunk::init(1, CHUNK_SIZE).list[0];
         for d in test_data {
             types::uuid_io::write(&d, chunk.addr);
             assert!(types::uuid_io::read(chunk.addr) == d);
@@ -151,12 +187,13 @@ mod string {
     use neb::ram::types;
     use neb::ram::chunk;
     use std::string::String;
+    use super::CHUNK_SIZE;
     #[test]
     fn test () {
         let test_data = vec![
             "", "ಬಾ ಇಲ್ಲಿ ಸಂಭವಿಸ", "中文测试文本", "Hello Test", "💖"
         ];
-        let chunk = &chunk::init(1, 2048).list[0];
+        let chunk = &chunk::init(1, CHUNK_SIZE).list[0];
         for d in test_data {
             let test_str = String::from(d);
             types::string_io::write(&test_str, chunk.addr);
