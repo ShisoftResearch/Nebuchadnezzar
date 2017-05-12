0c574107b8b3c82012b25f59c8438292baa9abc4
Jack Shi <shisoftgenius@gmail.com>
2017-05-12 20:33:10 +0800

    test cell write twice

--- tests/transaction.rs
+++ tests/transaction.rs
@@ -34,7 +34,7 @@ pub fn basic_wr () {
     data_map.insert(String::from("id"), Value::I64(100));
     data_map.insert(String::from("score"), Value::U64(70));
     data_map.insert(String::from("name"), Value::String(String::from("Jack")));
-    let cell_1 = Cell::new(schema.id, &Id::rand(), data_map);
+    let cell_1 = Cell::new(schema.id, &Id::rand(), data_map.clone());
     let cell_1_w_res = txn.write(&txn_id, &cell_1).unwrap().unwrap();
     match cell_1_w_res {
         TransactionExecResult::Accepted(()) => {},
@@ -50,4 +50,21 @@ pub fn basic_wr () {
         },
         _ => {panic!("read cell 1 not accepted {:?}", cell_1_w_res)}
     }
+    data_map.insert(String::from("score"), Value::U64(90));
+    let cell_1_w2 = Cell::new(schema.id, &cell_1.id(), data_map.clone());
+    let cell_1_w_res = txn.write(&txn_id, &cell_1_w2).unwrap().unwrap();
+    match cell_1_w_res {
+        TransactionExecResult::Accepted(()) => {panic!("Write existed cell should fail")}
+        TransactionExecResult::Error(WriteError::CellAlreadyExisted) => {}
+        _ => {panic!("Wrong feedback {:?}", cell_1_w_res)}
+    }
+    let cell_1_r_res = txn.read(&txn_id, &cell_1.id()).unwrap().unwrap();
+    match cell_1_r_res {
+        TransactionExecResult::Accepted(cell) => {
+            assert_eq!(cell.id(), cell_1.id());
+            assert_eq!(cell.data.Map().unwrap().get("score").unwrap().U64().unwrap(), 70);
+        },
+        _ => {panic!("read cell 1 not accepted {:?}", cell_1_w_res)}
+    }
+
 }
