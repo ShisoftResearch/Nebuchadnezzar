4384d824636613f412bdce9b53b080112f707191
Jack Shi <shisoftgenius@gmail.com>
2017-05-14 20:29:52 +0800

    more stress

--- tests/transaction.rs
+++ tests/transaction.rs
@@ -244,7 +244,7 @@ pub fn smoke_rw() {
     let mut cell_1 = Cell::new(schema.id, &Id::rand(), data_map_1.clone());
     server.chunks.write_cell(&mut cell_1);
     let cell_id = cell_1.id();
-    let thread_count = 10;
+    let thread_count = 30;
     let mut threads: Vec<thread::JoinHandle<()>> = Vec::with_capacity(thread_count);
     for _ in 0..thread_count {
         let txn = txn.clone();
