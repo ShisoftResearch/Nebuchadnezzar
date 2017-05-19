2c26d2b7d248e63d0a21cb2ba594aa0b0c69a308
Jack Shi <shisoftgenius@gmail.com>
2017-05-20 01:51:13 +0800

    client transaction proxy

--- src/client/transaction.rs
+++ src/client/transaction.rs
@@ -5,12 +5,22 @@ use ram::types::{Id};
 use std::sync::Arc;
 use std::io;
 
+use bifrost::rpc::RPCError;
+
 pub enum TxnError {
     CannotFindAServer,
     IoError(io::Error),
     CannotBegin,
     NotRealizable,
     TooManyRetry,
+    InternalError,
+    RPCError(RPCError),
+    ManagerError(TMError),
+    ReadError(ReadError),
+    WriteError(WriteError),
+    PrepareError(TMPrepareResult),
+    CommitError(EndResult),
+    AbortError(AbortResult)
 }
 
 pub struct Transaction {
@@ -20,26 +30,77 @@ pub struct Transaction {
 
 impl Transaction {
 
-    pub fn read(id: &Id) -> Result<Option<Cell>, TxnError> {
-        unimplemented!()
+    pub fn read(&self, id: &Id) -> Result<Option<Cell>, TxnError> {
+        match self.client.read(&self.tid, id) {
+            Ok(Ok(TxnExecResult::Accepted(cell))) => Ok(Some(cell)),
+            Ok(Ok(TxnExecResult::Rejected)) => Err(TxnError::NotRealizable),
+            Ok(Ok(TxnExecResult::Error(ReadError::CellDoesNotExisted))) => Ok(None),
+            Ok(Ok(_)) => Err(TxnError::InternalError),
+            Ok(Err(tme)) => Err(TxnError::ManagerError(tme)),
+            Ok(Ok(TxnExecResult::Error(re))) => Err(TxnError::ReadError(re)),
+            Err(e) => Err(TxnError::RPCError(e))
+        }
     }
-    pub fn write(cell: &Cell) -> Result<(), TxnError> {
-        unimplemented!()
+    pub fn write(&self, cell: &Cell) -> Result<(), TxnError> {
+        match self.client.write(&self.tid, cell) {
+            Ok(Ok(TxnExecResult::Accepted(()))) => Ok(()),
+            Ok(Ok(TxnExecResult::Rejected)) => Err(TxnError::NotRealizable),
+            Ok(Ok(TxnExecResult::Error(we))) => Err(TxnError::WriteError(we)),
+            Ok(Ok(_)) => Err(TxnError::InternalError),
+            Ok(Err(tme)) => Err(TxnError::ManagerError(tme)),
+            Err(e) => Err(TxnError::RPCError(e))
+        }
     }
-    pub fn update(cell: &Cell) -> Result<(), TxnError> {
-        unimplemented!()
+    pub fn update(&self, cell: &Cell) -> Result<(), TxnError> {
+        match self.client.update(&self.tid, cell) {
+            Ok(Ok(TxnExecResult::Accepted(()))) => Ok(()),
+            Ok(Ok(TxnExecResult::Rejected)) => Err(TxnError::NotRealizable),
+            Ok(Ok(TxnExecResult::Error(we))) => Err(TxnError::WriteError(we)),
+            Ok(Ok(_)) => Err(TxnError::InternalError),
+            Ok(Err(tme)) => Err(TxnError::ManagerError(tme)),
+            Err(e) => Err(TxnError::RPCError(e))
+        }
     }
-    pub fn remove(id: &Id) -> Result<(), TxnError> {
-        unimplemented!()
+    pub fn remove(&self, id: &Id) -> Result<(), TxnError> {
+        match self.client.remove(&self.tid, id) {
+            Ok(Ok(TxnExecResult::Accepted(()))) => Ok(()),
+            Ok(Ok(TxnExecResult::Rejected)) => Err(TxnError::NotRealizable),
+            Ok(Ok(TxnExecResult::Error(we))) => Err(TxnError::WriteError(we)),
+            Ok(Ok(_)) => Err(TxnError::InternalError),
+            Ok(Err(tme)) => Err(TxnError::ManagerError(tme)),
+            Err(e) => Err(TxnError::RPCError(e))
+        }
     }
 
-    pub fn prepare() -> Result<(), TxnError> {
-        unimplemented!()
+    pub fn prepare(&self) -> Result<(), TxnError> {
+        match self.client.prepare(&self.tid) {
+            Ok(Ok(TMPrepareResult::Success)) => Ok(()),
+            Ok(Ok(TMPrepareResult::DMPrepareError
+                  (DMPrepareResult::NotRealizable))) =>
+                Err(TxnError::NotRealizable),
+            Ok(Ok(TMPrepareResult::DMCommitError(
+                      DMCommitResult::CellChanged(_, _)))) =>
+                Err(TxnError::NotRealizable),
+            Ok(Ok(rpr)) => Err(TxnError::PrepareError(rpr)),
+            Ok(Err(tme)) => Err(TxnError::ManagerError(tme)),
+            Err(e) => Err(TxnError::RPCError(e))
+        }
     }
-    pub fn commit() -> Result<(), TxnError> {
-        unimplemented!()
+    pub fn commit(&self) -> Result<(), TxnError> {
+        match self.client.commit(&self.tid) {
+            Ok(Ok(EndResult::Success)) => Ok(()),
+            Ok(Ok(EndResult::SomeLocksNotReleased)) => Ok(()),
+            Ok(Ok(er)) => Err(TxnError::CommitError(er)),
+            Ok(Err(tme)) => Err(TxnError::ManagerError(tme)),
+            Err(e) => Err(TxnError::RPCError(e))
+        }
     }
-    pub fn abort() -> Result<(), TxnError> {
-        unimplemented!()
+    pub fn abort(&self) -> Result<(), TxnError> {
+        match self.client.abort(&self.tid) {
+            Ok(Ok(AbortResult::Success(_))) => Ok(()),
+            Ok(Ok(ar)) => Err(TxnError::AbortError(ar)),
+            Ok(Err(tme)) => Err(TxnError::ManagerError(tme)),
+            Err(e) => Err(TxnError::RPCError(e))
+        }
     }
 }
