2d0aae61a7e66b1d9c9ff0d44859724793a8b5d5
Jack Shi <shisoftgenius@gmail.com>
2017-05-20 00:55:25 +0800

    transaction loop

--- src/client/mod.rs
+++ src/client/mod.rs
@@ -3,8 +3,10 @@ use bifrost::conshash::{ConsistentHashing, CHError};
 use bifrost::raft::client::{RaftClient, ClientError};
 use bifrost::raft;
 
+use server::{transactions as txn_server};
 use ram::types::Id;
 use ram::cell::{Cell, ReadError, WriteError};
+use self::transaction::*;
 
 pub mod transaction;
 pub mod plain;
@@ -23,7 +25,7 @@ impl Client {
         match RaftClient::new(meta_servers, raft::DEFAULT_SERVICE_ID) {
             Ok(raft_client) => {
                 match ConsistentHashing::new_client(group, &raft_client) {
-                    Ok(chash) => Ok(Client {conshash: chash}), 
+                    Ok(chash) => Ok(Client {conshash: chash}),
                     Err(err) => Err(NebClientError::ConsistentHashtableError(err))
                 }
             },
@@ -42,8 +44,33 @@ impl Client {
     fn remove_cell(&self, key: &Id) -> Result<(), WriteError> {
         unimplemented!()
     }
-    fn transaction<TFN>(&self, func: TFN) -> Result<(), transaction::TxnError>
-        where TFN: Fn(transaction::Transaction) {
-        unimplemented!()
+    fn transaction<TFN>(&self, func: TFN) -> Result<(), TxnError>
+        where TFN: Fn(Transaction) -> Result<(), TxnError> {
+        let server_name = match self.conshash.rand_server() {
+            Some(name) => name,
+            None => return Err(TxnError::CannotFindAServer)
+        };
+        let txn_client = match txn_server::new_client(&server_name) {
+            Ok(client) => client,
+            Err(e) => return Err(TxnError::IoError(e))
+        };
+        let mut txn_id: txn_server::TxnId;
+        while true {
+            txn_id = match txn_client.begin() {
+                Ok(Ok(id)) => id,
+                _ => return Err(TxnError::CannotBegin)
+            };
+            let txn = Transaction{
+                tid: txn_id,
+                client: txn_client.clone()
+            };
+            let exec_result = func(txn);
+            match exec_result {
+                Ok(()) => return Ok(()),
+                Err(TxnError::NotRealizable) => continue,
+                Err(e) => return Err(e)
+            }
+        }
+        Ok(())
     }
 }
--- src/client/transaction.rs
+++ src/client/transaction.rs
@@ -1,17 +1,25 @@
 use server::transactions::TxnId;
+use server::transactions::*;
 use ram::cell::{Cell, ReadError, WriteError};
 use ram::types::{Id};
+use std::sync::Arc;
+use std::io;
 
 pub enum TxnError {
-
+    CannotFindAServer,
+    IoError(io::Error),
+    CannotBegin,
+    NotRealizable
 }
 
 pub struct Transaction {
-    tid: TxnId
+    pub tid: TxnId,
+    pub client: Arc<manager::SyncServiceClient>
 }
 
 impl Transaction {
-    pub fn read(id: &Id) -> Result<Cell, TxnError> {
+
+    pub fn read(id: &Id) -> Result<Option<Cell>, TxnError> {
         unimplemented!()
     }
     pub fn write(cell: &Cell) -> Result<(), TxnError> {
