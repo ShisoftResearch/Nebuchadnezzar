98ae6858d094f515a8ec40473f0f747f464e56f7
Jack Shi <shisoftgenius@gmail.com>
2017-05-28 19:34:18 +0800

    the real skew write test, passed

--- tests/client.rs
+++ tests/client.rs
@@ -7,6 +7,8 @@ use neb::client::transaction::TxnError;
 use std::thread;
 use std::sync::Arc;
 use env_logger;
+use std::time::Duration;
+use parking_lot::Mutex;
 
 use super::*;
 
@@ -77,7 +79,7 @@ pub fn general() {
 }
 
 #[test]
-pub fn write_skew() {
+pub fn multi_cell_update() {
     let server_addr = String::from("127.0.0.1:5401");
     let server = NebServer::new(ServerOptions {
         chunk_count: 1,
@@ -110,7 +112,7 @@ pub fn write_skew() {
     client.write_cell(&cell_1).unwrap().unwrap();
     client.read_cell(&cell_1.id()).unwrap().unwrap();
     let cell_1_id = cell_1.id();
-    let mut cell_2 = cell_1.clone(); // clone one to test write skew
+    let mut cell_2 = cell_1.clone();
     cell_2.set_id(&Id::rand());
     client.write_cell(&cell_2).unwrap().unwrap();
     client.read_cell(&cell_2.id()).unwrap().unwrap();
@@ -119,18 +121,24 @@ pub fn write_skew() {
     for i in 0..thread_count {
         let client = client.clone();
         threads.push(thread::spawn(move || {
+            let mut score_1 = 0;
+            let mut score_2 = 0;
             client.transaction(|ref mut txn| {
-                let cell_1_id = if i % 2 == 1 {&cell_1_id} else {&cell_2_id};
-                let cell_2_id = if i % 2 == 0 {&cell_1_id} else {&cell_2_id};
-                let mut cell_1 = txn.read(cell_1_id)?.unwrap();
-                let mut cell_2 = txn.read(cell_2_id)?.unwrap();
-                let mut score = cell_1.data.Map().unwrap().get("score").unwrap().U64().unwrap();
-                score += 1;
-                let mut data = cell_1.data.Map().unwrap().clone();
-                data.insert(String::from("score"), Value::U64(score));
-                cell_1.data = Value::Map(data);
+                let mut cell_1 = txn.read(&cell_1_id)?.unwrap();
+                let mut cell_2 = txn.read(&cell_2_id)?.unwrap();
+                score_1 = cell_1.data.Map().unwrap().get("score").unwrap().U64().unwrap();
+                score_2 = cell_2.data.Map().unwrap().get("score").unwrap().U64().unwrap();
+                score_1 += 1;
+                score_2 += 1;
+                let mut data_1 = cell_1.data.Map().unwrap().clone();
+                data_1.insert(String::from("score"), Value::U64(score_1));
+                cell_1.data = Value::Map(data_1);
+                let mut data_2 = cell_2.data.Map().unwrap().clone();
+                data_2.insert(String::from("score"), Value::U64(score_2));
+                cell_2.data = Value::Map(data_2);
                 txn.update(&cell_1)?;
                 txn.update(&cell_2)?;
+                assert_eq!(txn.changes, 4);
                 Ok(())
             }).unwrap();
         }));
@@ -142,6 +150,84 @@ pub fn write_skew() {
     let mut cell_2_r = client.read_cell(&cell_2_id).unwrap().unwrap();
     let cell_1_score = cell_1_r.data.Map().unwrap().get("score").unwrap().U64().unwrap();
     let cell_2_score = cell_2_r.data.Map().unwrap().get("score").unwrap().U64().unwrap();
-    println!("{} / {}", cell_1_score, cell_2_score);
-    assert_eq!(cell_1_score + cell_2_score, thread_count as u64);
+    assert_eq!(cell_1_score + cell_2_score, (thread_count * 2) as u64);
+}
+
+#[test]
+pub fn write_skew() {
+    let server_addr = String::from("127.0.0.1:5402");
+    let server = NebServer::new(ServerOptions {
+        chunk_count: 1,
+        memory_size: 16 * 1024 * 1024,
+        standalone: false,
+        is_meta: true,
+        meta_members: vec!(server_addr.clone()),
+        address: server_addr.clone(),
+        backup_storage: None,
+        meta_storage: None,
+        group_name: String::from("test"),
+    }).unwrap();
+    let mut schema = Schema {
+        id: 1,
+        name: String::from("test"),
+        key_field: None,
+        fields: default_fields()
+    };
+    let client = Arc::new(client::Client::new(
+        &server.rpc, &vec!(server_addr),
+        &String::from("test")).unwrap());
+    let thread_count = 100;
+    client.new_schema(&mut schema).unwrap();
+    let mut data_map = Map::<String, Value>::new();
+    data_map.insert(String::from("id"), Value::I64(100));
+    data_map.insert(String::from("score"), Value::U64(0));
+    data_map.insert(String::from("name"), Value::String(String::from("Jack")));
+    let cell_1 = Cell::new(schema.id, &Id::rand(), data_map.clone());
+    client.write_cell(&cell_1).unwrap().unwrap();
+    client.read_cell(&cell_1.id()).unwrap().unwrap();
+    let cell_1_id = cell_1.id();
+    let client_c1 = client.clone();
+    let skew_tried = Arc::new(Mutex::new(0));
+    let normal_tried = Arc::new(Mutex::new(0));
+
+    let skew_tried_c = skew_tried.clone();
+    let normal_tried_c = normal_tried.clone();
+
+    let t1 = thread::spawn(move || {
+        client_c1.transaction(|ref mut txn| {
+            *skew_tried_c.lock() += 1;
+            let mut cell_1 = txn.read(&cell_1_id)?.unwrap();
+            let mut score_1 = cell_1.data.Map().unwrap().get("score").unwrap().U64().unwrap();
+            thread::sleep(Duration::new(5, 0)); // wait 5 secs to let late write occur
+            score_1 += 1;
+            let mut data_1 = cell_1.data.Map().unwrap().clone();
+            data_1.insert(String::from("score"), Value::U64(score_1));
+            cell_1.data = Value::Map(data_1);
+            txn.update(&cell_1)?;
+            Ok(())
+        });
+    });
+    let client_c2 = client.clone();
+    let t2 = thread::spawn(move || {
+        client_c2.transaction(|ref mut txn| {
+            thread::sleep(Duration::new(1, 0));
+            *normal_tried_c.lock() += 1;
+            let mut cell_1 = txn.read(&cell_1_id)?.unwrap();
+            let mut score_1 = cell_1.data.Map().unwrap().get("score").unwrap().U64().unwrap();
+            score_1 += 1;
+            let mut data_1 = cell_1.data.Map().unwrap().clone();
+            data_1.insert(String::from("score"), Value::U64(score_1));
+            cell_1.data = Value::Map(data_1);
+            txn.update(&cell_1)?;
+            Ok(())
+        });
+    });
+    t2.join();
+    t1.join();
+    let mut cell_1_r = client.read_cell(&cell_1_id).unwrap().unwrap();
+    let cell_1_score = cell_1_r.data.Map().unwrap().get("score").unwrap().U64().unwrap();
+    assert_eq!(cell_1_score, 2);
+//    assert_eq!(*skew_tried.lock(), 2);
+//    assert_eq!(*normal_tried.lock(), 1);
+    println!("Skew tried {}, normal tried {}", *skew_tried.lock(), *normal_tried.lock());
 }
