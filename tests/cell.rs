b9c39cdfd9939e6064ffc8beaba39bbfc4d2fefe
Jack Shi <shisoftgenius@gmail.com>
2017-07-23 01:46:41 +0800

    test for dynamic cell #4

--- tests/cell.rs
+++ tests/cell.rs
@@ -29,21 +29,14 @@ pub fn cell_rw () {
         is_dynamic: false
     };
     let mut data_map = Map::new();
-    data_map.insert(&String::from("id"), Value::I64(100));
-    data_map.insert(&String::from("score"), Value::U64(70));
-    data_map.insert(&String::from("name"), Value::String(String::from("Jack")));
+    data_map.insert("id", Value::I64(100));
+    data_map.insert("score", Value::U64(70));
+    data_map.insert("name", Value::String(String::from("Jack")));
     let mut data = Value::Map(data_map);
     let chunk = &Chunks::new_dummy(1, CHUNK_SIZE).list[0];
     chunk.meta.schemas.new_schema(schema.clone());
     let mut cell = Cell {
         header: Header::new(0, schema.id, &id1),
-//        Header {
-//            version: 1,
-//            size: 0,
-//            schema: 0,
-//            hash: 1,
-//            partition: 1,
-//        },
         data: data
     };
     let mut loc = cell.write_to_chunk(&chunk);
@@ -57,19 +50,12 @@ pub fn cell_rw () {
         assert_eq!(stored_cell.data["score"].U64().unwrap(), 70);
     }
     data_map = Map::new();
-    data_map.insert(&String::from("id"), Value::I64(2));
-    data_map.insert(&String::from("score"), Value::U64(80));
-    data_map.insert(&String::from("name"), Value::String(String::from("John")));
+    data_map.insert("id", Value::I64(2));
+    data_map.insert("score", Value::U64(80));
+    data_map.insert("name", Value::String(String::from("John")));
     data = Value::Map(data_map);
     cell = Cell {
         header: Header::new(0, schema.id, &id2),
-//        Header {
-//            version: 1,
-//            size: 0,
-//            schema: 0,
-//            hash: 2,
-//            partition: 1,
-//        },
         data: data
     };
     loc = cell.write_to_chunk(&chunk);
@@ -89,5 +75,65 @@ pub fn cell_rw () {
         assert_eq!(stored_cell.data["id"].I64().unwrap(), 100);
         assert_eq!(stored_cell.data["name"].String().unwrap(), "Jack");
         assert_eq!(stored_cell.data["score"].U64().unwrap(), 70);
+    }
+}
+
+#[test]
+pub fn dynamic() {
+    let fields = default_fields();
+    let id1 = Id::new(1, 1);
+    let id2 = Id::new(1, 2);
+    let mut schema = Schema {
+        id: 1,
+        name: String::from("dummy"),
+        key_field: None,
+        str_key_field: None,
+        fields: fields,
+        is_dynamic: true
+    };
+    let mut data_map = Map::new();
+    data_map.insert("id", Value::I64(100));
+    data_map.insert("score", Value::U64(70));
+    data_map.insert("name", Value::String(String::from("Jack")));
+    data_map.insert("year", Value::U16(2010));
+    data_map.insert("major", Value::String(String::from("CS")));
+    let mut data = Value::Map(data_map);
+    let chunk = &Chunks::new_dummy(1, CHUNK_SIZE).list[0];
+    chunk.meta.schemas.new_schema(schema.clone());
+    let mut cell = Cell {
+        header: Header::new(0, schema.id, &id1),
+        data: data
+    };
+    let mut loc = cell.write_to_chunk(&chunk);
+    let cell_1_ptr = loc.unwrap();
+    {
+        let stored_cell = Cell::from_chunk_raw(cell_1_ptr, &chunk).unwrap();
+        assert!(stored_cell.header.size > (4 + HEADER_SIZE) as u32);
+        assert!(stored_cell.header.size > (4 + HEADER_SIZE) as u32);
+        assert_eq!(stored_cell.data["id"].I64().unwrap(), 100);
+        assert_eq!(stored_cell.data["name"].String().unwrap(), "Jack");
+        assert_eq!(stored_cell.data["score"].U64().unwrap(), 70);
+        assert_eq!(stored_cell.data["year"].U16().unwrap(), 2010);
+        assert_eq!(stored_cell.data["major"].String().unwrap(), "CS");
+    }
+
+    data_map = Map::new();
+    data_map.insert("id", Value::I64(2));
+    data_map.insert("score", Value::U64(80));
+    data_map.insert("name", Value::String(String::from("John")));
+    data = Value::Map(data_map);
+    cell = Cell {
+        header: Header::new(0, schema.id, &id2),
+        data: data
+    };
+    loc = cell.write_to_chunk(&chunk);
+    let cell_2_ptr = loc.unwrap();
+    {
+        let stored_cell = Cell::from_chunk_raw(cell_2_ptr, &chunk).unwrap();
+        assert!(stored_cell.header.size > (4 + HEADER_SIZE) as u32);
+        assert_eq!(stored_cell.data["id"].I64().unwrap(), 2);
+        assert_eq!(stored_cell.data["score"].U64().unwrap(), 80);
+        assert_eq!(stored_cell.data["name"].String().unwrap(), "John");
+        assert!(stored_cell.data["major"].String().is_none());
     }
 }
