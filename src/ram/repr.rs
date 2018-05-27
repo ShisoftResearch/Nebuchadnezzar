f27757543f988d597519a370559a04c16e6c4175
Hao Shi <shisoftgenius@gmail.com>
2018-05-27 08:42:25 +0800

    impl decoder

--- src/ram/repr.rs
+++ src/ram/repr.rs
@@ -1,16 +1,16 @@
-use byteorder::{LittleEndian, WriteBytesExt};
+use byteorder::{LittleEndian, WriteBytesExt, ByteOrder};
 use std::ptr;
 use libc;
 
 bitflags! {
-    pub struct EntryTypes: u8 {
+    pub struct EntryType: u8 {
         const Cell = 0b00010000;
         const Tomestone = 0b00110000;
     }
 }
 
-pub struct Entry {
-    entry_flag: u8,
+pub struct EntryHeader {
+    entry_type: EntryType,
     entry_length: u32,
 }
 
@@ -30,20 +30,20 @@ fn count_len_bytes(len: u32) -> u8 {
     return bytes;
 }
 
-fn compress_len(len: u32, bytes: &mut[u8]) {
-    bytes.write_u32::<LittleEndian>(len).unwrap();
+fn encode_len(len: u32, bytes: &mut[u8]) {
+    LittleEndian::write_u32(bytes, len);
 }
 
-impl Entry {
-    pub fn encode_to<W>(mut pos: usize, entry_type: EntryTypes, content_len: u32, write_content: W)
+impl EntryHeader {
+    pub fn encode_to<W>(mut pos: usize, entry_type: EntryType, content_len: u32, write_content: W)
         where W: Fn(usize)
     {
         let len_bytes_count = count_len_bytes(content_len);
         let len_bytes_count_usize = len_bytes_count as usize;
         let flag_byte = len_bytes_count | entry_type.bits;
-        let mut len_bytes = Vec::with_capacity(len_bytes_count_usize);
-        compress_len(content_len, &mut len_bytes);
-        let raw_len_bytes= Box::into_raw(len_bytes.into_boxed_slice());
+        let mut len_bytes = [0u8; 4];
+        encode_len(content_len, &mut len_bytes);
+        let raw_len_bytes= Box::into_raw(box len_bytes);
         unsafe {
             // write entry flags
             *(pos as *mut u8) = flag_byte;
@@ -60,13 +60,26 @@ impl Entry {
         }
     }
 
-    pub fn decode_from(mut pos: usize) -> (Entry, usize) {
+    // Returns the entry header and content position
+    pub fn decode_from(mut pos: usize) -> (EntryHeader, usize) {
         unsafe {
             let flag_byte = *(pos as *mut u8);
+            pos += 1;
             let entry_type_bits = 0b11110000 & flag_byte;
-            let entry_type = EntryTypes::from_bits(entry_type_bits);
+            let entry_type = EntryType::from_bits(entry_type_bits).unwrap();
             let entry_bytes_len = 0b00001111 & flag_byte;
-
+            let entry_bytes_len_usize = entry_bytes_len as usize;
+            let raw_len_bytes= Box::into_raw(box [0u8; 4]);
+            libc::memmove(
+                pos as *mut libc::c_void,
+                raw_len_bytes as *mut libc::c_void,
+                entry_bytes_len_usize);
+            let entry_length = LittleEndian::read_u32(&*Box::from_raw(raw_len_bytes));
+            let enrty = EntryHeader {
+                entry_type,
+                entry_length
+            };
+            (entry, pos + 1 + entry_bytes_len_usize)
         }
     }
 }
