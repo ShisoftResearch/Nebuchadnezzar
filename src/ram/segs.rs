f99dccaae1708b9855d6df33a8bf91f130b562b6
Hao Shi <shisoftgenius@gmail.com>
2018-10-24 23:33:34 +0800

    overcome the overflow, agaion

--- src/ram/segs.rs
+++ src/ram/segs.rs
@@ -106,9 +106,14 @@ impl Segment {
 
     // dead space plus tombstone spaces
     pub fn total_dead_space(&self) -> u32 {
-        let dead_tombstones_space = self.dead_tombstones.load(Ordering::Relaxed) * TOMBSTONE_SIZE_U32;
-        let dead_cells_space = self.dead_space();
-        return dead_tombstones_space + dead_cells_space;
+        let total_dead_space = self.total_dead_space();
+        let used_space = self.used_spaces();
+        if total_dead_space <= used_space {
+            used_space - total_dead_space
+        } else {
+            warn!("living space check error, used {}, dead {}", used_space, total_dead_space);
+            0
+        }
     }
 
     pub fn used_spaces(&self) -> u32 {
