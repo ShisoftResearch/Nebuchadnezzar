9d58fcabe28a393a5261dde3b3f71277ca420d6e
Hao Shi <shisoftgenius@gmail.com>
2018-10-31 10:24:33 +0800

    mutate segment reference to releve lock preesure on segment map

--- src/ram/chunk.rs
+++ src/ram/chunk.rs
@@ -323,12 +323,7 @@ impl Chunk {
             seg_index_guard.remove(&seg_original_pos);
         }
         seg_index_guard.insert(segment_addr, segment_id);
-        if self.segs
-            .get_mut(&segment_id)
-            .map(|mut seg| *seg = (&segment).clone())
-            .is_some()
-            { return }
-        self.segs.insert(segment_id, segment);
+        self.segs.alter(segment_id, |_| Some(segment));
     }
 
     pub fn remove_segment(&self, segment_id: u64) {
