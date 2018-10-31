0bdbd0c5298ff82fbe8ba47646596e4902255fae
Hao Shi <shisoftgenius@gmail.com>
2018-10-31 10:46:49 +0800

    Revert "mutate segment reference to releve lock preesure on segment map"
    This reverts commit 9d58fca

--- src/ram/chunk.rs
+++ src/ram/chunk.rs
@@ -323,7 +323,12 @@ impl Chunk {
             seg_index_guard.remove(&seg_original_pos);
         }
         seg_index_guard.insert(segment_addr, segment_id);
-        self.segs.alter(segment_id, |_| Some(segment));
+        if self.segs
+            .get_mut(&segment_id)
+            .map(|mut seg| *seg = (&segment).clone())
+            .is_some()
+            { return }
+        self.segs.insert(segment_id, segment);
     }
 
     pub fn remove_segment(&self, segment_id: u64) {
