d4dbb3616c8b28fdd90226d11fa81b74d49ed407
Hao Shi <shisoftgenius@gmail.com>
2018-12-14 15:44:44 +0800

    found break point but lacking infomation, verbose log

--- src/index/btree/internal.rs
+++ src/index/btree/internal.rs
@@ -281,10 +281,11 @@ impl InNode {
                 let mut new_right_keys = EntryKeySlice::init();
                 let mut new_right_ptrs = NodePtrCellSlice::init();
 
-                let pivot_key = self.keys[right_ptr_pos - 1].to_owned();
+                let pivot_key_pos = right_ptr_pos - 1;
+                let pivot_key = self.keys[pivot_key_pos].to_owned();
                 let mut new_left_keys_len = 0;
                 let mut new_right_keys_len = 0;
-                debug_assert!(pivot_key > smallvec!(0));
+                debug_assert!(pivot_key > smallvec!(0), "Current pivot key at {} is empty, now keys are {:?}", pivot_key_pos, self.keys);
                 for (i, key) in chain(
                     chain(
                         left_innode.keys[..left_innode.len].iter_mut(),
