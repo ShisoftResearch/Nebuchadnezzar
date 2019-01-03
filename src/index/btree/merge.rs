ca986137eb0a0f851da496791353b98e5b1a59f1
Hao Shi <shisoftgenius@gmail.com>
2019-01-03 19:17:20 +0800

    merge internal nodes

--- src/index/btree/merge.rs
+++ src/index/btree/merge.rs
@@ -50,16 +50,14 @@ pub fn merge_into_tree_node<KS, PS>(
             // this is a oneshot action.
             // after the merge, it will return all new inserted new pages to upper level
             let mut merging_pos = 0;
-            let mut current_page = sub_node;
-            let mut new_pages: Vec<NodeCellRef> = vec![];
+            let mut current_guard = write_node::<KS, PS>(&sub_node);
+            let mut new_pages = vec![];
             // merge by pages
             while merging_pos < keys.len() {
-                let mut current_guard = write_node::<KS, PS>(&current_page);
                 let start_key = &keys[merging_pos];
                 let mut target_page_guard = write_key_page(current_guard, start_key);
-                let key_upper_bound = target_page_guard
-                    .right_ref()
-                    .map(|r| write_non_empty(write_node::<KS, PS>(r)).first_key().clone());
+                let mut right_guard = write_non_empty(write_node::<KS, PS>(target_page_guard.right_ref().unwrap()));
+                let key_upper_bound = if right_guard.is_none() { None } else { Some(right_guard.first_key().clone()) };
                 let selection = keys[merging_pos..].iter()
                     .filter(|k| match &key_upper_bound {
                         &Some(ref upper) => k < &upper,
@@ -68,6 +66,30 @@ pub fn merge_into_tree_node<KS, PS>(
                     .take(KS::slice_len() - target_page_guard.len())
                     .collect_vec();
                 target_page_guard.extnode_mut().merge_sort(selection.as_slice());
+                merging_pos += selection.len();
+                if merging_pos >= keys.len() {
+                    break;
+                }
+                let next_insert_key = &keys[merging_pos];
+                if key_upper_bound.is_some() && next_insert_key < key_upper_bound.as_ref().unwrap() {
+                    // trigger a split and put the split node into a cache
+                    let insert_pos = target_page_guard.search(&keys[merging_pos]);
+                    let target_node_ref = target_page_guard.node_ref().clone();
+                    let (new_node, pivot) = target_page_guard
+                        .extnode_mut()
+                        .split_insert(
+                            next_insert_key.clone(),
+                            insert_pos,
+                            &target_node_ref,
+                            &mut right_guard,
+                            tree);
+                    merging_pos += 1;
+                    current_guard = write_node(&new_node);
+                    new_pages.push((pivot, new_node));
+                } else {
+                    debug_assert!(!right_guard.is_none());
+                    current_guard = right_guard;
+                }
             }
             unimplemented!()
         }
