7fc662084d18aee2fa5d51afe6e58b0e1b4f9188
Hao Shi <shisoftgenius@gmail.com>
2019-03-16 02:34:44 +0800

    fix

--- src/index/btree/search.rs
+++ src/index/btree/search.rs
@@ -48,7 +48,7 @@ where
                 );
                 if ordering == Ordering::Backward {
                     debug!("found cursor pos {} for backwards, will be corrected", pos);
-                    if pos >= n.len || pos > 0 && &n.keys.as_slice_immute()[pos] != key {
+                    if pos > 0 && (pos >= n.len || &n.keys.as_slice_immute()[pos] != key) {
                         pos -= 1;
                     }
                     debug!("cursor pos have been corrected to {}", pos);
