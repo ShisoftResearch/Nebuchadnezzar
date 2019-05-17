7dc8636167d5cc2a141c42348a6f02d65b526346
Hao Shi <shisoftgenius@gmail.com>
2019-05-17 10:26:37 +0800

    get of cascading read node

--- src/index/btree/split.rs
+++ src/index/btree/split.rs
@@ -17,15 +17,25 @@ where
     PS: Slice<NodeCellRef> + 'static,
 {
     debug!("Searching for mid key for split");
-    read_node(node_ref, |node_handler: &NodeReadHandler<KS, PS>| {
+    enum R {
+        Result(Option<EntryKey>),
+        SubSearch(NodeCellRef),
+        None
+    }
+    let r = read_node(node_ref, |node_handler: &NodeReadHandler<KS, PS>| {
         let node = &**node_handler;
         match node {
-            &NodeData::External(ref n) => Some(n.keys.as_slice_immute()[n.len / 2].clone()),
-            &NodeData::Internal(ref n) => mid_key::<KS, PS>(&n.ptrs.as_slice_immute()[n.len / 2]),
-            &NodeData::Empty(ref n) => mid_key::<KS, PS>(&n.right),
-            &NodeData::None => None,
+            &NodeData::External(ref n) => R::Result(Some(n.keys.as_slice_immute()[n.len / 2].clone())),
+            &NodeData::Internal(ref n) => R::SubSearch(n.ptrs.as_slice_immute()[n.len / 2].clone()) ,
+            &NodeData::Empty(ref n) => R::SubSearch(n.right.clone()),
+            &NodeData::None => R::None,
         }
-    })
+    });
+    match r {
+        R::Result(r) => r,
+        R::SubSearch(r) =>  mid_key::<KS, PS>(&r),
+        R::None => None
+    }
 }
 
 pub fn remove_to_right<KS, PS>(node_ref: &NodeCellRef, start_key: &EntryKey) -> usize
