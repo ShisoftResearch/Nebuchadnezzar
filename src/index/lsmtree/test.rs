a89bbbf91e555bcf4b24925e0ef3cf393045bb53
Hao Shi <shisoftgenius@gmail.com>
2019-05-18 17:08:25 +0800

    check insertions

--- src/index/lsmtree/test.rs
+++ src/index/lsmtree/test.rs
@@ -68,12 +68,6 @@ pub fn insertions() {
         tree.insert(key);
     });
 
-    dump_trees(&*tree, "stage_1_before_insertion");
-    for _ in 0..20 {
-        tree.check_and_merge();
-    }
-    dump_trees(&*tree, "stage_1_after_insertion");
-
     debug!("Start point search validations");
     (0..num).collect::<Vec<_>>().par_iter().for_each(|i| {
         let i = *i;
@@ -86,24 +80,14 @@ pub fn insertions() {
         assert_eq!(cursor.current(), Some(&key), "{}", i);
     });
 
-    LSMTree::start_sentinel(&tree);
+    dump_trees(&*tree, "stage_1_before_insertion");
+    for _ in 0..50 {
+        tree.check_and_merge();
+    }
+    dump_trees(&*tree, "stage_1_after_insertion");
 
-    let mut test_data = (num..num * 2).collect_vec();
-    thread_rng().shuffle(test_data.as_mut_slice());
-    test_data.par_iter().for_each(|i| {
-        let i = *i;
-        let id = Id::new(0, i);
-        let key_slice = u64_to_slice(i);
-        let mut key = SmallVec::from_slice(&key_slice);
-        key_with_id(&mut key, &id);
-        tree.insert(key);
-    });
-
-    dump_trees(&*tree, "stage_2_after_insertion");
-    thread::sleep(Duration::new(30, 0));
-    dump_trees(&*tree, "stage_2_waited_insertion");
-
-    (0..num * 2).collect::<Vec<_>>().par_iter().for_each(|i| {
+    debug!("Start point search validations");
+    (0..num).collect::<Vec<_>>().par_iter().for_each(|i| {
         let i = *i;
         let id = Id::new(0, i);
         let key_slice = u64_to_slice(i);
