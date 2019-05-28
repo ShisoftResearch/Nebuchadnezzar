151e6ccb7b76d8b15b3ea8519ded76197c9f6ae9
Hao Shi <shisoftgenius@gmail.com>
2019-05-28 20:45:52 +0800

    fix reference

--- src/index/btree/level.rs
+++ src/index/btree/level.rs
@@ -370,7 +370,7 @@ where
                     } else {
                         // can be updated, set the new key
                         debug!("perform key update {:?}", new_key);
-                        debug_assert!(new_key > MIN_ENTRY_KEY, "new key is empty at {}", i);
+                        debug_assert!(&new_key > &*MIN_ENTRY_KEY, "new key is empty at {}", i);
                         innode.keys.as_slice()[i - 1] = new_key;
                     }
                 }
