07aaa45f3b72aa457f0eb8fb47179fe3f9cc9d51
Hao Shi <shisoftgenius@gmail.com>
2019-06-21 14:20:54 +0800

    inspect and update placement

--- src/index/lsmtree/client.rs
+++ src/index/lsmtree/client.rs
@@ -1,5 +1,5 @@
 use index::lsmtree::placement::sm::client::SMClient as PlacementClient;
-use index::lsmtree::placement::sm::Placement as PlacementMeta;
+use index::lsmtree::placement::sm::{Placement as PlacementMeta, QueryError};
 use index::lsmtree::service::AsyncServiceClient;
 use index::EntryKey;
 use linked_hash_map::LinkedHashMap;
@@ -9,6 +9,8 @@ use server::NebServer;
 use std::collections::btree_map::BTreeMap;
 use std::sync::atomic::{AtomicUsize, Ordering::Relaxed};
 use std::sync::Arc;
+use futures::prelude::*;
+use index::lsmtree::split::placement_client;
 
 pub struct IndexEntry {
     id: Id,
@@ -28,8 +30,25 @@ pub struct Placement {
 
 pub struct LSMTreeClient {
     counter: AtomicUsize,
-    placements: RwLock<BTreeMap<EntryKey, Placement>>,
+    placements: RwLock<BTreeMap<Vec<u8>, Placement>>,
     cursors: LinkedHashMap<usize, Cursor>,
     placement_client: PlacementClient,
     neb: Arc<NebServer>,
+}
+
+impl LSMTreeClient {
+    fn inspect_placement(&self, id: &Id) {
+        match self.placement_client.get(id).wait().unwrap() {
+            Ok(placement) => {
+                let mut placements = self.placements.write();
+                let rpc_client = placement_client(id, &self.neb).wait().unwrap();
+                placements.insert(placement.starts.clone(), Placement {
+                    meta: placement,
+                    client: rpc_client
+                });
+            },
+            Err(QueryError::PlacementNotFound) => panic!("cannot find a suitable placement"),
+            Err(e) => panic!("{:?}", e)
+        }
+    }
 }
