ae6b4a424e82573653ee3d3bb993753704c2b0cd
Hao Shi <shisoftgenius@gmail.com>
2020-09-10 17:23:52 -0400

    sketching master tree for ranged index

--- src/index/ranged/sm.rs
+++ src/index/ranged/sm.rs
@@ -1,8 +1,49 @@
 use super::trees::*;
+use std::collections::BTreeMap;
 use crate::ram::types::Id;
+use futures::prelude::*;
+use bifrost::raft::state_machine::StateMachineCtl;
+use bifrost::utils;
+use bifrost::raft::RaftService;
+
+pub struct MasterTreeSM {
+    tree: BTreeMap<EntryKey, Id>,
+    sm_id: u64
+}
 
 raft_state_machine! {
     def qry locate_key(entry: EntryKey) -> Id;
-    def cmd split(tree: Id, new_tree: Id, pivot: EntryKey);
+    def cmd split(new_tree: Id, pivot: EntryKey);
     // No subscription for clients
+}
+
+impl StateMachineCmds for MasterTreeSM {
+    fn locate_key(&self, entry: EntryKey) -> BoxFuture<Id> {
+        future::ready(
+            self.tree.range(entry..)
+            .next()
+            .map(|(_, id)| *id)
+            .unwrap()
+        ).boxed()
+    }
+
+    fn split(&mut self, new_tree: Id, pivot: EntryKey) -> BoxFuture<()> {
+        self.tree.insert(pivot, new_tree);
+        future::ready(()).boxed()
+    }
+}
+
+impl StateMachineCtl for MasterTreeSM {
+    raft_sm_complete!();
+    fn id(&self) -> u64 {
+        self.sm_id
+    }
+    fn snapshot(&self) -> Option<Vec<u8>> {
+        Some(utils::serde::serialize(&self.tree))
+    }
+    fn recover(&mut self, data: Vec<u8>) -> BoxFuture<()> {
+        let tree = utils::serde::deserialize(&data).unwrap();
+        self.tree = tree; 
+        future::ready(()).boxed()
+    }
 }
