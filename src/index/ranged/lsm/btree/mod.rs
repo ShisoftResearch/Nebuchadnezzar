8dd666c7a76d2258f9c42f1b321461124d7de9e1
Hao Shi <shisoftgenius@gmail.com>
2020-12-20 23:23:27 -0500

    need better merge

--- src/index/ranged/lsm/btree/mod.rs
+++ src/index/ranged/lsm/btree/mod.rs
@@ -190,7 +190,10 @@ where
             let new_root_len = root_new_pages.len();
             debug_assert!(
                 new_root_len + 1 < KS::slice_len(),
-                "Radical merge split, cannot handle this for now (need to split more than once)"
+                "Radical merge split, cannot handle this for now (need to split more than once), {} to {}, num keys {}", 
+                new_root_len + 1,
+                KS::slice_len(),
+                keys_len
             );
             let mut new_in_root: Box<InNode<KS, PS>> = InNode::new(new_root_len, max_entry_key());
             new_in_root.ptrs.as_slice()[0] = root.clone();
