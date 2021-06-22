cc933cfe303624f74ca4cf5ec397cfa95f1d6984
Hao Shi <shisoftgenius@gmail.com>
2021-06-22 01:26:32 -0400

    scaffolding statistics from chunk

--- src/query/statistics/mod.rs
+++ src/query/statistics/mod.rs
@@ -1,9 +1,10 @@
 use std::{collections::HashMap, sync::Arc};
-use lightning::map::ObjectMap;
+use itertools::Itertools;
+use lightning::map::{Map, ObjectMap};
 
-use dovahkiin::types::OwnedValue;
+use dovahkiin::types::{OwnedValue, SharedValue};
 
-use crate::ram::chunk::Chunk;
+use crate::ram::{cell::{CellHeader, header_from_chunk_raw, select_from_chunk_raw}, chunk::Chunk};
 
 pub mod sm;
 
@@ -18,9 +19,45 @@ pub struct ChunkStatistics {
 
 impl ChunkStatistics {
   pub fn from_chunk(chunk: &Chunk) -> Self {
-    // for (hash, ptr) in chunk.cell_index.entries() {
-      
-    // }
+    for (hash, _) in chunk.cell_index.entries() {
+      let loc = if let Ok(ptr) = chunk.location_for_read(hash as u64) {
+        ptr
+      } else {
+        trace!("Cannot obtain cell lock {} for statistics", hash);
+        continue;
+      };
+      match header_from_chunk_raw(*loc) {
+        Ok((header, _)) => {
+          let schema_id = header.schema;
+          if let Some(schema) = chunk.meta.schemas.get(&schema_id) {
+            let fields = schema.index_fields.keys().cloned().collect_vec();
+            if let Ok(partial_cell) = select_from_chunk_raw(*loc, chunk, fields.as_slice()) {
+              drop(loc); // Release lock for the cell early
+              let field_array = if fields.len() == 1 {
+                vec![partial_cell]
+              } else if let SharedValue::Array(arr) = partial_cell {
+                arr    
+              } else {
+                error!("Cannot decode partial cell for statistics {:?}", partial_cell);
+                continue;
+              };
+              for (i, val) in field_array.into_iter().enumerate() {
+                if val == SharedValue::Null || val == SharedValue::NA {
+                  continue;
+                }
+                let field_id = fields[i];
+                // TODO: build the histogram
+              }
+            }
+          } else {
+            warn!("Cannot get schema {} for statistics", schema_id);
+          }
+        },
+        Err(e) => {
+          warn!("Failed to read {} for statistics, error: {:?}", hash, e);
+        }
+      }
+    }
     unimplemented!()
   }
 }
