91e91000d87e4ef9bb1c8737ec3042c1329e556d
Hao Shi <shisoftgenius@gmail.com>
2021-07-10 13:15:07 -0400

    refactoring

--- src/query/statistics/mod.rs
+++ src/query/statistics/mod.rs
@@ -2,12 +2,11 @@ use itertools::Itertools;
 use lightning::map::{Map, ObjectMap};
 use rayon::prelude::*;
 use std::{
-    collections::{BTreeSet, HashMap, HashSet},
+    collections::{HashMap, HashSet},
     sync::Arc,
 };
-use tokio_stream::StreamExt;
 
-use dovahkiin::types::{OwnedValue, SharedValue};
+use dovahkiin::types::SharedValue;
 
 use crate::ram::{
     cell::{header_from_chunk_raw, select_from_chunk_raw},
@@ -46,91 +45,7 @@ impl ChunkStatistics {
             .collect_vec();
         let partitations: Vec<_> = histogram_partitations
             .into_par_iter()
-            .map(|partitation| {
-                // Build exact histogram for each of the partitation and then approximate overall histogram
-                let mut sizes = HashMap::new();
-                let mut segs = HashMap::new();
-                let mut counts = HashMap::new();
-                let mut exact_accumlators = HashMap::new();
-                let partitation_size = partitation.len();
-                for (hash, _) in partitation {
-                    let loc = if let Ok(ptr) = chunk.location_for_read(hash as u64) {
-                        ptr
-                    } else {
-                        trace!("Cannot obtain cell lock {} for statistics", hash);
-                        continue;
-                    };
-                    match header_from_chunk_raw(*loc) {
-                        Ok((header, _, entry_header)) => {
-                            let cell_size = entry_header.content_length as usize;
-                            let cell_seg = chunk.allocator.id_by_addr(*loc);
-                            let schema_id = header.schema;
-                            if let Some(schema) = chunk.meta.schemas.get(&schema_id) {
-                                let fields = schema.index_fields.keys().cloned().collect_vec();
-                                if let Ok(partial_cell) =
-                                    select_from_chunk_raw(*loc, chunk, fields.as_slice())
-                                {
-                                    let field_array = if fields.len() == 1 {
-                                        vec![partial_cell]
-                                    } else if let SharedValue::Array(arr) = partial_cell {
-                                        arr
-                                    } else {
-                                        error!(
-                                            "Cannot decode partial cell for statistics {:?}",
-                                            partial_cell
-                                        );
-                                        continue;
-                                    };
-                                    for (i, val) in field_array.into_iter().enumerate() {
-                                        if val == SharedValue::Null || val == SharedValue::NA {
-                                            continue;
-                                        }
-                                        let field_id = fields[i];
-                                        exact_accumlators
-                                            .entry(schema_id)
-                                            .or_insert_with(|| HashMap::new())
-                                            .entry(field_id)
-                                            .or_insert_with(|| Vec::with_capacity(partitation_size))
-                                            .push(val.feature());
-                                    }
-                                    *counts.entry(schema_id).or_insert(0) += 1;
-                                    *sizes.entry(schema_id).or_insert(0) += cell_size;
-                                    segs.entry(schema_id)
-                                        .or_insert_with(|| HashSet::new())
-                                        .insert(cell_seg);
-                                }
-                            } else {
-                                warn!("Cannot get schema {} for statistics", schema_id);
-                            }
-                        }
-                        Err(e) => {
-                            warn!("Failed to read {} for statistics, error: {:?}", hash, e);
-                        }
-                    }
-                }
-                let histograms: HashMap<_, _> = exact_accumlators
-                    .into_iter()
-                    .map(|(schema_id, schema_histograms)| {
-                        let compiled_histograms = schema_histograms
-                            .into_iter()
-                            .map(|(field, mut items)| {
-                                items.sort();
-                                let depth = items.len() / HISTOGRAM_PARTITATION_BUCKETS;
-                                let mut histogram = (0..HISTOGRAM_PARTITATION_BUCKETS)
-                                    .map(|tile| items[tile * depth])
-                                    .collect_vec();
-                                let last_item = &items[items.len() - 1];
-                                if histogram.last().unwrap() != last_item {
-                                    histogram.push(*last_item);
-                                }
-                                (field, (histogram, items.len(), depth))
-                            })
-                            .collect::<HashMap<_, _>>();
-                        (schema_id, compiled_histograms)
-                    })
-                    .collect::<HashMap<_, _>>();
-                (sizes, segs, counts, histograms)
-            })
+            .map(|partitation| build_partitation_histogram(partitation, chunk))
             .collect();
         let schema_ids: Vec<_> = partitations
             .iter()
@@ -216,9 +131,102 @@ impl ChunkStatistics {
             schema_statistics.insert(&(*schema_id as usize), Arc::new(statistics));
         }
         Self {
-            schemas: schema_statistics
+            schemas: schema_statistics,
         }
     }
+}
+
+fn build_partitation_histogram(
+    partitation: Vec<(usize, usize)>,
+    chunk: &Chunk,
+) -> (
+    HashMap<u32, usize>,
+    HashMap<u32, HashSet<usize>>,
+    HashMap<u32, usize>,
+    HashMap<u32, HashMap<u64, (Vec<HistogramKey>, usize, usize)>>,
+) {
+    // Build exact histogram for each of the partitation and then approximate overall histogram
+    let mut sizes = HashMap::new();
+    let mut segs = HashMap::new();
+    let mut counts = HashMap::new();
+    let mut exact_accumlators = HashMap::new();
+    let partitation_size = partitation.len();
+    for (hash, _) in partitation {
+        let loc = if let Ok(ptr) = chunk.location_for_read(hash as u64) {
+            ptr
+        } else {
+            trace!("Cannot obtain cell lock {} for statistics", hash);
+            continue;
+        };
+        match header_from_chunk_raw(*loc) {
+            Ok((header, _, entry_header)) => {
+                let cell_size = entry_header.content_length as usize;
+                let cell_seg = chunk.allocator.id_by_addr(*loc);
+                let schema_id = header.schema;
+                if let Some(schema) = chunk.meta.schemas.get(&schema_id) {
+                    let fields = schema.index_fields.keys().cloned().collect_vec();
+                    if let Ok(partial_cell) = select_from_chunk_raw(*loc, chunk, fields.as_slice())
+                    {
+                        let field_array = if fields.len() == 1 {
+                            vec![partial_cell]
+                        } else if let SharedValue::Array(arr) = partial_cell {
+                            arr
+                        } else {
+                            error!(
+                                "Cannot decode partial cell for statistics {:?}",
+                                partial_cell
+                            );
+                            continue;
+                        };
+                        for (i, val) in field_array.into_iter().enumerate() {
+                            if val == SharedValue::Null || val == SharedValue::NA {
+                                continue;
+                            }
+                            let field_id = fields[i];
+                            exact_accumlators
+                                .entry(schema_id)
+                                .or_insert_with(|| HashMap::new())
+                                .entry(field_id)
+                                .or_insert_with(|| Vec::with_capacity(partitation_size))
+                                .push(val.feature());
+                        }
+                        *counts.entry(schema_id).or_insert(0) += 1;
+                        *sizes.entry(schema_id).or_insert(0) += cell_size;
+                        segs.entry(schema_id)
+                            .or_insert_with(|| HashSet::new())
+                            .insert(cell_seg);
+                    }
+                } else {
+                    warn!("Cannot get schema {} for statistics", schema_id);
+                }
+            }
+            Err(e) => {
+                warn!("Failed to read {} for statistics, error: {:?}", hash, e);
+            }
+        }
+    }
+    let histograms: HashMap<_, _> = exact_accumlators
+        .into_iter()
+        .map(|(schema_id, schema_histograms)| {
+            let compiled_histograms = schema_histograms
+                .into_iter()
+                .map(|(field, mut items)| {
+                    items.sort();
+                    let depth = items.len() / HISTOGRAM_PARTITATION_BUCKETS;
+                    let mut histogram = (0..HISTOGRAM_PARTITATION_BUCKETS)
+                        .map(|tile| items[tile * depth])
+                        .collect_vec();
+                    let last_item = &items[items.len() - 1];
+                    if histogram.last().unwrap() != last_item {
+                        histogram.push(*last_item);
+                    }
+                    (field, (histogram, items.len(), depth))
+                })
+                .collect::<HashMap<_, _>>();
+            (schema_id, compiled_histograms)
+        })
+        .collect::<HashMap<_, _>>();
+    (sizes, segs, counts, histograms)
 }
 
 fn build_histogram(
