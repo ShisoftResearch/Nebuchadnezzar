17b0f3c0a1c72fed8c6439c628b1f6d9a96c2105
Hao Shi <shisoftgenius@gmail.com>
2024-05-04 12:20:34 +0000

    cleanup

--- src/exec/mod.rs
+++ src/exec/mod.rs
@@ -1,9 +1,5 @@
-// Intermediate presentation and AST for user input
-pub mod ir;
 // Directed acyclic graph for query plannning
 pub mod dag;
-// Instruction sequence for execution
-pub mod seq;
 // Shared symbols
 pub mod symbols;
 // Shared functions
