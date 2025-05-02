4e12b6723eede5a07269b4892cc3ac0630e71f69
Hao Shi <shisoftgenius@gmail.com>
2025-05-02 11:39:48 +0000

    get rid of expensive LRU impl

--- src/server/transactions/data_site.rs
+++ src/server/transactions/data_site.rs
@@ -11,6 +11,7 @@ use bifrost::vector_clock::StandardVectorClock;
 use bifrost_plugins::hash_ident;
 use futures::future::BoxFuture;
 use futures::stream::FuturesUnordered;
+use lightning::linked_map::LinkedHashMap;
 use lightning::lru_cache::LRUCache;
 use lightning::map::Map;
 use lightning::map::PtrHashMap as LFMap;
@@ -59,7 +60,7 @@ impl CellHistory {
 
 pub struct DataManager {
     cells: LFMap<Id, Arc<Mutex<CellMeta>>>,
-    cell_lru: LRUCache<Id, i64>,
+    cell_lru: LinkedHashMap<Id, i64>,
     txns: LFMap<TxnId, Arc<Mutex<Transaction>>>,
     txns_sorted: Mutex<BTreeSet<TxnId>>,
     managers: LFMap<u64, Arc<manager::AsyncServiceClient>>,
@@ -91,7 +92,7 @@ impl DataManager {
         let cleanup_signal = Arc::new(AtomicBool::new(false));
         let manager = Arc::new(Self {
             cells: LFMap::with_capacity(256),
-            cell_lru: LRUCache::new(256),
+            cell_lru: LinkedHashMap::with_capacity(256),
             txns: LFMap::with_capacity(128),
             txns_sorted: Mutex::new(BTreeSet::new()),
             managers: LFMap::with_capacity(16),
@@ -134,7 +135,7 @@ impl DataManager {
     fn cell_meta_mutex(&self, id: &Id) -> CellMetaMutex {
         {
             let lru = &self.cell_lru;
-            lru.get(id, |_| Some(get_time()), |_, _| {});
+            lru.insert_back(*id, get_time());
         }
         self.cells.get_or_insert(*id, || {
             Arc::new(Mutex::new(CellMeta {
@@ -229,7 +230,7 @@ impl DataManager {
         let now = get_time();
         let mut cell_to_evict = Vec::new();
         let mut need_break = false;
-        for (cell_id, timestamp) in self.cell_lru.iter() {
+        for (cell_id, timestamp) in self.cell_lru.iter_front() {
             if let Some(cell_meta) = self.cells.get(&cell_id) {
                 let meta = cell_meta.lock();
                 if meta.write < oldest_transaction
